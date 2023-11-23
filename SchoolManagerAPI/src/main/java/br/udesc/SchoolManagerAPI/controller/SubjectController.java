3604276b31aca102f31c4f44967b89ff5e0945a7
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-11-23 20:17:50 -0300

    chore: recupera endpoint removido

--- SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/controller/SubjectController.java
+++ SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/controller/SubjectController.java
@@ -1,5 +1,7 @@
 package br.udesc.SchoolManagerAPI.controller;
 
+import br.udesc.SchoolManagerAPI.domain.subject.Subject;
+import br.udesc.SchoolManagerAPI.domain.subject.SubjectRepository;
 import br.udesc.SchoolManagerAPI.domain.subject.SubjectService;
 import br.udesc.SchoolManagerAPI.domain.subject.dto.CreateSubjectDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +11,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/subjects")
+@RequestMapping("/subject")
 public class SubjectController {
 
     @Autowired
     private SubjectService subjectService;
+    @Autowired
+    private SubjectRepository subjectRepository;
 
     @PostMapping
     public ResponseEntity create(@RequestBody CreateSubjectDTO subjectDTO) {
         subjectService.save(subjectDTO.getName());
 
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Subject>> getAll() {
+        List<Subject> subjectList = subjectRepository.findAll();
+
+        return ResponseEntity.ok(subjectList);
     }
 }
