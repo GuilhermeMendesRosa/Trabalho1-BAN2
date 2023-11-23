50341f3becbb49c211ac9fd5ba5d9c90afedca63
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-11-23 12:50:36 -0300

    chore: ajustando Repository

--- SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/domain/subject/SubjectRepository.java
+++ SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/domain/subject/SubjectRepository.java
@@ -1,6 +1,8 @@
 package br.udesc.SchoolManagerAPI.domain.subject;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectRepository extends Neo4jRepository<Subject, Long> {
 }
