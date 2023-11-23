8f2cb7f402d7438bba1dd36849a0d1f9589ff199
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-11-22 23:46:41 -0300

    feat: conexão NEo4j

--- SchoolManagerAPI/src/main/resources/db/migration/V2__insert_subjects.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO subjects
-    (name, created_at, updated_at, version)
-VALUES
-    ('Inglês', CONVERT_TZ(NOW(),'SYSTEM','America/Sao_Paulo'), CONVERT_TZ(NOW(),'SYSTEM','America/Sao_Paulo'), 0);
-
-INSERT INTO subjects
-    (name, created_at, updated_at, version)
-VALUES
-    ('Matemática', CONVERT_TZ(NOW(),'SYSTEM','America/Sao_Paulo'), CONVERT_TZ(NOW(),'SYSTEM','America/Sao_Paulo'), 0);
