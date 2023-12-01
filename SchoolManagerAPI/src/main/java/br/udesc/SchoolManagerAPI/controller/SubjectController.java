b7716a267ee95b52185c93611a1e69cebc3ef5b3
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-11-30 23:52:19 -0300

    feat: endpoint para salvar matéria

--- SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/controller/SubjectController.java
+++ SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/controller/SubjectController.java
@@ -2,25 +2,35 @@ package br.udesc.SchoolManagerAPI.controller;
 
 import br.udesc.SchoolManagerAPI.domain.subject.Subject;
 import br.udesc.SchoolManagerAPI.domain.subject.SubjectRepository;
+import br.udesc.SchoolManagerAPI.domain.subject.SubjectService;
+import br.udesc.SchoolManagerAPI.domain.subject.dto.SubjectDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/subject")
 public class SubjectController {
+
     @Autowired
     private SubjectRepository subjectRepository;
+
+    @Autowired
+    private SubjectService subjectService;
 
     @GetMapping
     public ResponseEntity<List<Subject>> getAll() {
         List<Subject> all = subjectRepository.findAll();
 
         return ResponseEntity.ok(all);
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody SubjectDTO subjectDTO) {
+        subjectService.save(subjectDTO.getName());
+        return ResponseEntity.ok().build();
     }
 
 }
---
+++ SchoolManagerAPI/src/main/java/br/udesc/SchoolManagerAPI/domain/subject/SubjectService.java
@@ -0,0 +1,17 @@
+package br.udesc.SchoolManagerAPI.domain.subject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubjectService {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    public void save(String name) {
+        Subject subject = new Subject();
+        subject.setName(name);
+        subjectRepository.save(subject);
+    }
+}
